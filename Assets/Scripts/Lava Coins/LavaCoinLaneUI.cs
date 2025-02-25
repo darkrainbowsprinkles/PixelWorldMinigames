b3e54d60bc2986070036a53281719b63784d0430
ChoiBeomgyuItBoy <octaviortegan@hotmail.com>
2025-02-25 04:05:18 -0600

    Activate lava routine

---
+++ Assets/Scripts/Lava Coins/LavaCoinLaneUI.cs
@@ -0,0 +1,156 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace PixelWorld.FabulousFred
+{
+    public class LavaCoinLaneUI : MonoBehaviour
+    {
+        [SerializeField] Transform coinsContainer;
+        [SerializeField] TMP_Text scoreText;
+        [SerializeField] TMP_Text lavaWarningText;
+        [SerializeField] float lavaDuration = 5;
+        [SerializeField] float reenableDelay = 0.5f; 
+        Button[] coins;
+        int score = 0;
+        int collectedCoins = 0;
+        int totalCoins;
+        bool lavaActive = false;
+        Button lastSelectedCoin;
+
+        void Start()
+        {
+            FillLavaCoins();
+            StartCoroutine(ActivateLavaRoutine());
+            HandlePressedEvent(gameObject, a => LavaStep());
+            UpdateScore(0);
+        }
+
+        void FillLavaCoins()
+        {
+            totalCoins = coinsContainer.childCount;
+            coins = new Button[totalCoins];
+
+            for(int i = 0; i < totalCoins; i++)
+            {
+                Button coin = coinsContainer.GetChild(i).GetComponent<Button>();
+                coins[i] = coin;
+                HandlePressedEvent(coin.gameObject, a => OnCoinPressed(coin));
+            }
+        }
+
+        void HandlePressedEvent(GameObject gameObject, UnityAction<BaseEventData> action)
+        {
+            EventTrigger trigger = gameObject.AddComponent<EventTrigger>();
+            EventTrigger.Entry entry = new();
+
+            entry.eventID = EventTriggerType.PointerDown;
+            entry.callback.AddListener(action);
+
+            trigger.triggers.Add(entry);
+        }
+
+        void OnCoinPressed(Button coin)
+        {
+            coin.image.enabled = false;
+            collectedCoins++;
+            lastSelectedCoin = coin;
+
+            UpdateScore(1);
+
+            if(collectedCoins >= totalCoins / 3)
+            {
+                collectedCoins = 0; 
+                StartCoroutine(ReenableCoinsRoutine());
+            }
+        }
+
+        void UpdateScore(int scoreToAdd)
+        {
+            score += scoreToAdd;
+            scoreText.text = score.ToString();
+        }
+
+        IEnumerator ReenableCoinsRoutine()
+        {
+            foreach(var coin in coins)
+            {
+                if(!coin.image.enabled)
+                {
+                    yield return new WaitForSeconds(reenableDelay);
+                    coin.image.enabled = true;
+                }
+            }
+        }
+
+        IEnumerator ActivateLavaRoutine()
+        {
+            yield return StartCoroutine(CountDownRoutine());
+
+            lavaActive = true;
+
+            Image image = GetComponent<Image>();
+            Color originalColor = image.color;
+
+            image.color = Color.red;
+            image.raycastTarget = true;
+
+            DisableUnselectedCoins();
+
+            yield return new WaitForSeconds(lavaDuration);
+
+            EnableAllCoins();
+
+            image.raycastTarget = false;
+            image.color = originalColor;
+
+            lavaActive = false;
+        }
+
+        void DisableUnselectedCoins()
+        {
+            foreach(var coin in coins)
+            {
+                if(coin == lastSelectedCoin)
+                {
+                    coin.image.raycastTarget = false;
+                    coin.image.enabled = true;
+                    continue;
+                }
+
+                coin.image.enabled = false;
+            }
+        }
+
+        void EnableAllCoins()
+        {
+            foreach(var coin in coins)
+            {
+                coin.image.raycastTarget = true;
+                coin.image.enabled = true;
+            }
+        }
+
+        IEnumerator CountDownRoutine()
+        {
+            lavaWarningText.text = "3!";
+            yield return new WaitForSeconds(1);
+            lavaWarningText.text = "2!";
+            yield return new WaitForSeconds(1);
+            lavaWarningText.text = "1!";
+            yield return new WaitForSeconds(1);
+            lavaWarningText.text = "LAVA!";
+        }
+
+        void LavaStep()
+        {
+            if(lavaActive)
+            {
+                UpdateScore(-1);
+            }
+        }
+    }
+}
